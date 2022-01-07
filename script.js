f57efde08582a9ab43a652b1b728fac88e15fdf2
DavideVin01 <davidevinciguerradv01@gmail.com>
2022-01-07 15:48:54 +0100

    if i % 3 > print "FIzz"

--- script.js
+++ script.js
@@ -19,6 +19,9 @@ Provare a stampare la lista in pagina invece che in console.
 
 // #1 - Scrivere un programma che stampi i numeri da 1 a 100
 // #2 - Creare una variabile d'appoggio dove stampare i numeri
+// #3 SE multiplo di 3 > Stampo Fizz al posto del numero
+// #5 SE multiplo di 5 > Stampo Buzz al posto del numero
+// #6 SE multiplo di 3 e 5 > Stampo FizzBuzz al posto del numero
 
 let printNumbers = document.getElementById('result');
 
@@ -27,6 +30,10 @@ let numbers = ''; // Variabile d'appoggio
 for (let i = 1; i <= 100; i++) {
     console.log(i);
     numbers = numbers + i;
-}
+    if (i % 3 == 0) {
+        console.log("Fizz");
+    }
 
+
+}
 printNumbers.innerHTML = numbers;
