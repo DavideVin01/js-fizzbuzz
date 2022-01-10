8e28c52e1502c67be7aaa2546d325903559053e1
DavideVin01 <davidevinciguerradv01@gmail.com>
2022-01-10 09:52:18 +0100

    Removes unused content

--- script.js
+++ script.js
@@ -48,12 +48,12 @@ let numbers = ''; // Variabile d'appoggio
 // Second Case - Number won't get printed if the number (i) is divisible by 3 or 5
 
 for (let i = 1; i <= 100; i++) {
-    if (i % 3 == 0 && i % 5 == 0) {
-        console.log('FizzBuzz' + ' ')
-    } else if (i % 5 == 0) {
-        console.log('Buzz' + ' ')
-    } else if (i % 3 == 0) {
-        console.log('Fizz' + ' ')
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz')
+    } else if (i % 5 === 0) {
+        console.log('Buzz')
+    } else if (i % 3 === 0) {
+        console.log('Fizz')
     } else {
         console.log(i);
     }
